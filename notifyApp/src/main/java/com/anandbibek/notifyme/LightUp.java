2170322b26f8bc2d4b69d85b9669006ec529a0d4
anandbibek <ananda.bibek@gmail.com>
2014-03-24 00:08:38 +0530

    Initial pocket mode. check proximity sensor for 10secs if in pocket -- show notif if taken out in 10secs -- register screen_on listener and release sensor if not taken out

--- notifyApp/src/main/java/com/anandbibek/notifyme/LightUp.java
+++ notifyApp/src/main/java/com/anandbibek/notifyme/LightUp.java
@@ -18,16 +18,18 @@
 package com.anandbibek.notifyme;
 
 import android.app.Activity;
-import android.app.KeyguardManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.PowerManager;
+import android.os.Handler;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.WindowManager.LayoutParams;
 
@@ -35,14 +37,28 @@ public class LightUp extends Activity implements SensorEventListener {
 
 	Prefs prefs;
 	WaitForLight wFL;
-	boolean running;
+	boolean running, countdown;
     boolean mCovered;
     private SensorManager mSensorManager;
+    private Handler handler;
+
+    private BroadcastReceiver receiver = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context arg0, Intent arg1) {
+            if( ((TelephonyManager)arg0.getSystemService(Context.TELEPHONY_SERVICE)).getCallState() != 0 ){
+                arg0.unregisterReceiver(this);
+                return;
+            }
+            startActivity(new Intent(arg0, NotificationActivity.class ).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+            arg0.unregisterReceiver(this);
+        }
+    };
+
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+        countdown = false;
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         if(mSensorManager != null)
             mSensorManager.registerListener(this,mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
@@ -58,32 +74,60 @@ public class LightUp extends Activity implements SensorEventListener {
 			startActivity(intent);
 		}else
 			super.onNewIntent(intent);
+        //TODO handle Handler
 	}
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        mSensorManager.unregisterListener(this);
-        mSensorManager = null;
-
-        Log.d("Sensor value", sensorEvent.values[0]+"");
-        if( sensorEvent.values[0] < 2 /*cm*/)
+        //Log.d("Sensor value ", sensorEvent.values[0]+"");
+        if( sensorEvent.values[0] < 2 /*cm*/) {
             //device in pocket
-            mCovered=true;
+            //Log.d("In pocket ", System.currentTimeMillis()+"");
+            if(!countdown){
+                mCovered=true;
+                handlePocket(this);
+                //Log.d("Handler started ", System.currentTimeMillis()+"");
+                countdown=true;
+            }
+        }
         else {
+            //Log.d("Out of pocket ", System.currentTimeMillis()+"");
+            mSensorManager.unregisterListener(this);
+            mSensorManager = null;
+            if(countdown) {
+                handler.removeCallbacksAndMessages(null);
+                //Log.d("Handler stopped ", System.currentTimeMillis()+"");
+            }
             mCovered = false;
             getWindow().addFlags(LayoutParams.FLAG_TURN_SCREEN_ON);
+            getWindow().addFlags(LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+            prefs = new Prefs(this);
+            wFL = new WaitForLight();
+            wFL.execute();
+            running = true;
         }
-
-        getWindow().addFlags(LayoutParams.FLAG_SHOW_WHEN_LOCKED);
-        prefs = new Prefs(this);
-        wFL = new WaitForLight();
-        wFL.execute();
-        running = true;
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
         //unused
+    }
+
+    public void handlePocket(final Context context){
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSensorManager.unregisterListener((SensorEventListener) context);
+                mSensorManager = null;
+                //Log.d("Unregistered ", System.currentTimeMillis()+"");
+                IntentFilter iFilter = new IntentFilter();
+                iFilter.addAction(Intent.ACTION_SCREEN_ON);
+                registerReceiver(receiver, iFilter);
+                //Log.d("Registered  broadcast receiver",System.currentTimeMillis()+"");
+            }
+        }, 10000 /* We need this delay to get new flags applied */
+        );
     }
 
 	
